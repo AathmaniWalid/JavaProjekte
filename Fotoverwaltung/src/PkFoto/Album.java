a597652628f1dcc12afb311eb5591757ad55ef9f
AathmaniWalid <walidaathmani2003@gmail.com>
2024-11-15 18:37:47 +0100

    Array mit einer typisierte Liste ersetzen und alle methoden ändern

--- Fotoverwaltung/src/PkFoto/Album.java
+++ Fotoverwaltung/src/PkFoto/Album.java
@@ -1,57 +1,47 @@
 package PkFoto;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Album extends Fachobjekt{
 
 private String besitzer;
-private Foto[] fotos= new Foto[2];
-private int anzFotos;
+private List<Foto> fotos = new ArrayList<>();
+
 public Album(String name, String besitzer) {
 	    super(name);
         this.besitzer = besitzer;
-        this.anzFotos = 0;
 } 
-
-public Foto[] getFotos() {
-	
+public List<Foto> getFotos() {
         return fotos;
 }
 public void addFoto(Foto foto) {
-        if(anzFotos == fotos.length)
-        	fotos = Arrays.copyOf(fotos, fotos.length*2);
-        
-        fotos[anzFotos] = foto; 
-        anzFotos++; 
-  
+        fotos.add(foto);
 }
-
-
 
 public String getBesitzer() {
         return besitzer;
 }
 
-
-
 public String toString() {
     String result = super.toString() + "\nName: " + getName() + "\nBesitzer: " + besitzer + "\n";
-    
-    for(int i = 0; i< fotos.length; i++) {
-    	 if (fotos[i] != null) {
-    	result += "\n=== Foto " + (i + 1) + " ===\n";
-    	result += fotos[i].toString();}
-    	}
-    return result;
+    int fotoNr=1;
+    for(Foto foto : fotos) {
+    	result += "\n=== Foto " + fotoNr + " ===\n";
+    	result += foto.toString();
+        fotoNr++;
     }
+    	
+    return result;
+}
 
 public void drucke() {
-    
+    int fotoNr=1;
     System.out.println("Name: "+getName());
     System.out.println("Besitzer: "+besitzer);
-    for (int i = 0; i < fotos.length; i++) {
-    	   if (fotos[i] != null) {
-    		   System.out.println("\n=== Foto " + (i + 1) + " ===");
-               fotos[i].drucke();}
-    }
-  }
+    for (Foto foto: fotos) {
+    	System.out.println("\n=== Foto " + fotoNr + " ===");
+        fotoNr++;
+        foto.drucke();
+        }
+   }
 }
